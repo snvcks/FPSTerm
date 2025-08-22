1f7730d324080ab8ec52d37ee617bae2a5b6fcd0
= <=>
2025-08-22 13:35:44 +1000

    experiment with ncurses

--- test.cpp
+++ test.cpp
@@ -3,9 +3,24 @@
 int main()
 {	
 	initscr();			/* Start curses mode 		  */
-	printw("Hello World !!!");	/* Print Hello World		  */
-	refresh();			/* Print it on to the real screen */
-	getch();			/* Wait for user input */
+	
+    printw("Hello World !!!");	/* Print Hello World		  */
+	
+    refresh();			/* Print it on to the real screen */
+
+
+	int c = getch();			/* Wait for user input */
+
+
+    int y = 10;
+    int x = 10;
+    //move
+    move(y,x);
+
+    printw("Meow %d",c);
+
+    getch();
+
 	endwin();			/* End curses mode		  */
 
 	return 0;
